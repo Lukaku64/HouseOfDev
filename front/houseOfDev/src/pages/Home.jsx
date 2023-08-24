ca03bef0e92b9e66ccb1a53044ca56e5fbfabf11
Lucas Flores <federicotoro467@gmail.com>
2023-08-24 16:56:38 -0300

    creacion de home como de store

---
+++ front/houseOfDev/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <p>Nose que otra cosa poner</p>
+    </>
+  );
+}
+
+export default Home;
