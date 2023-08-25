8e1cc14f24e7f0e877110544469fa8785d63c4d7
Lucas Flores <federicotoro467@gmail.com>
2023-08-25 10:38:53 -0300

    modificacion esquema con bcrypt

--- api/schemas/User.js
+++ api/schemas/User.js
@@ -12,6 +12,37 @@ const userSchema = new Schema({
   role: { type: String, enum: ["admin", "agente", "user"], default: "user" },
 });
 
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  const passwordRegex = /^(?=.*[A-Z]).{8,}$/;
+
+  if (!passwordRegex.test(user.password)) {
+    const error = new Error(
+      "La contraseña debe contener al menos una letra mayúscula y tener al menos 8 caracteres"
+    );
+    return next(error);
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
