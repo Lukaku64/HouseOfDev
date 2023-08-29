d372eb9355ee626b3c6e66fb1e0a4b9aace80a79
Lucas Flores <federicotoro467@gmail.com>
2023-08-29 12:52:41 -0300

    creacion de rutas de propiedades

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = express.Router();
 const userRoutes = require("./user.routes");
+const propertiesRoutes = require("./properties.routes");
 
 routes.use("/user", userRoutes);
+routes.use("/properties", propertiesRoutes);
 
 module.exports = routes;
