bb5e3bb24b49b7b6498d9a44d38c0eb6df52f91f
Lucas Flores <federicotoro467@gmail.com>
2023-09-01 02:41:10 -0300

    agregado de properties

--- front/houseOfDev/src/pages/Home.jsx
+++ front/houseOfDev/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Properties from "../components/Properties";
 
 function Home() {
   return (
     <>
       <Navbar />
       <p>Nose que otra cosa poner</p>
+      <Properties />
     </>
   );
 }
