42f54700ef0868b6458eb7b34f0c0ff617c652e8
Lucas Flores <federicotoro467@gmail.com>
2023-09-06 12:50:18 -0300

    creacion paginas de venta y compra

---
+++ front/houseOfDev/src/pages/OnSale.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Properties from "../components/Properties";
+
+function OnSale() {
+  return (
+    <>
+      <Navbar />
+      <h1>Propiedades en Venta</h1>
+      <Properties />
+    </>
+  );
+}
+
+export default OnSale;
