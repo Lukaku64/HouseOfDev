1dc9e68577be289c242b1e021a6946be5d469d40
Lucas Flores <federicotoro467@gmail.com>
2023-09-12 18:03:44 -0300

    modificacion de esquema de base de datos

--- api/schemas/Estate.js
+++ api/schemas/Estate.js
@@ -15,8 +15,11 @@ const estateSchema = new Schema({
     {
       user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
       comments: String,
+      stars: { type: Number, min: 0, max: 5 },
+      createdAt: { type: Date, default: Date.now },
     },
   ],
+  averageRating: { type: Number, default: 0 },
   date: [
     {
       date: String,
