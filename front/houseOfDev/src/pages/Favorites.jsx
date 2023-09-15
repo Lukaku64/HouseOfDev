f9a4a74e3f6aa2ef2aaac84df57fa5d53b6f6e47
Lucas Flores <federicotoro467@gmail.com>
2023-09-15 13:31:13 -0300

    modificacion de bugs en properties

--- front/houseOfDev/src/pages/Favorites.jsx
+++ front/houseOfDev/src/pages/Favorites.jsx
@@ -33,7 +33,6 @@ function Favorites() {
       .catch((err) => console.log(err));
   };
 
-  console.log(propertiesUser);
   return (
     <>
       <Navbar />
@@ -47,56 +46,60 @@ function Favorites() {
           <div className="w-5/6 border-b-blue mb-2"></div>
         </div>
         <div>
-          <ul className="grid grid-cols-2 xl:grid-cols-3">
-            {propertiesUser.favourties?.map((e) => (
-              <li key={e._id} className="m-3 p-4">
-                <div className="flex border-blue-2">
-                  <div className="w-1/3 border-r-blue">
-                    {e?.images[0] ? (
-                      <img src={e?.images} alt="Imagen" />
-                    ) : (
-                      <img src={imgNot} alt="Imagen no disponible" />
-                    )}
+          {Object.values(propertiesUser).length >= 1 ? (
+            <ul className="grid grid-cols-2 xl:grid-cols-3">
+              {propertiesUser.favourties?.map((e) => (
+                <li key={e._id} className="m-3 p-4">
+                  <div className="flex border-blue-2">
+                    <div className="w-1/3 border-r-blue">
+                      {e?.images[0] ? (
+                        <img src={e?.images} alt="Imagen" />
+                      ) : (
+                        <img src={imgNot} alt="Imagen no disponible" />
+                      )}
+                    </div>
+                    <div className="w-2/3">
+                      <div className="flex">
+                        <span className="border-rb-blue text-blue w-2/5 p-2">
+                          ${e.price}
+                        </span>
+                        <span className="border-b-blue text-blue w-3/5 p-2">
+                          {e.address}
+                        </span>
+                      </div>
+                      <div className="flex">
+                        <span className="border-rb-blue text-blue w-1/3 p-2">
+                          {e.squareMeter}
+                        </span>
+                        <span className="border-rb-blue text-blue w-1/3 p-2">
+                          {e.bedroom} dorm.
+                        </span>
+                        <span className="border-b-blue text-blue w-1/3 p-2">
+                          {parseInt(e.bathroom) > 1
+                            ? `${e.bathroom} baños`
+                            : `${e.bathroom} baño`}
+                        </span>
+                      </div>
+                      <p className="border-b-blue text-blue p-2">
+                        {e.description}
+                      </p>
+                      <div className="flex justify-end p-2">
+                        <button
+                          type="submit"
+                          className="w-48 border-blue text-blue text-sm py-2 m-1 rounded-full"
+                          onClick={(event) => handleDeleteFav(event, e._id)}
+                        >
+                          Eliminar de Favoritos
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="w-2/3">
-                    <div className="flex">
-                      <span className="border-rb-blue text-blue w-2/5 p-2">
-                        ${e.price}
-                      </span>
-                      <span className="border-b-blue text-blue w-3/5 p-2">
-                        {e.address}
-                      </span>
-                    </div>
-                    <div className="flex">
-                      <span className="border-rb-blue text-blue w-1/3 p-2">
-                        {e.squareMeter}
-                      </span>
-                      <span className="border-rb-blue text-blue w-1/3 p-2">
-                        {e.bedroom} dorm.
-                      </span>
-                      <span className="border-b-blue text-blue w-1/3 p-2">
-                        {parseInt(e.bathroom) > 1
-                          ? `${e.bathroom} baños`
-                          : `${e.bathroom} baño`}
-                      </span>
-                    </div>
-                    <p className="border-b-blue text-blue p-2">
-                      {e.description}
-                    </p>
-                    <div className="flex justify-end p-2">
-                      <button
-                        type="submit"
-                        className="w-48 border-blue text-blue text-sm py-2 m-1 rounded-full"
-                        onClick={(event) => handleDeleteFav(event, e._id)}
-                      >
-                        Eliminar de Favoritos
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <h1>Al parecer no tienes ninguna propiedad favorita</h1>
+          )}
         </div>
       </div>
     </>
